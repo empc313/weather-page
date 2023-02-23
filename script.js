543eff666de83f9b6242897467442970e12377ea
Farhod Akramov <farhod@Farhods-MacBook-Air.local>
2023-02-23 12:54:29 -0700

    initial commit

--- script.js
+++ script.js
@@ -1,63 +1,55 @@
 // Set global variables,Open Weather Maps API Key
-var city = "Denver"
-var APIKey = "22c9b7b3a4e8a1f95ebc91f63d87daea"
+var city = "Denver";
+var APIKey = "22c9b7b3a4e8a1f95ebc91f63d87daea";
 
-var searchEl = $("#search-form")
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" +APIKey;
-var weatherEl = $("#weatherConditions")
-var subMainWeatherEl = $("#subDescription")
-var icon = $(".icon")
+var searchEl = $("#search-form");
+var queryURL =
+  "https://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&appid=" +
+  APIKey;
+var weatherEl = $("#weatherConditions");
+var subMainWeatherEl = $("#subDescription");
+var icon = $(".icon");
 
+function userInputCitySubmit(event) {
+  event.preventDefault();
+  var userCityInput = $('input[name="city-input"]').val();
 
-
-
-function userInputCitySubmit(event){
-    event.preventDefault();
-var userCityInput = $('input[name="city-input"]').val();
-
-
-city = userCityInput;
-queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" +APIKey;
-console.log(city)
-getApi()
+  city = userCityInput;
+  queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    APIKey;
+  console.log(city);
+  getApi();
 }
 
+console.log(city);
 
-console.log(city)
-
-
-function getApi(){
-    fetch(queryURL)
+function getApi() {
+  fetch(queryURL)
     .then(function (response) {
-        console.log(response.status) 
-            if(response.status == 404){
-                var errorDisplay = response.status
-                console.log(errorDisplay)
-            }
-        return response.json()
+      console.log(response.status);
+      if (response.status == 404) {
+        var errorDisplay = response.status;
+        console.log(errorDisplay);
+      }
+      return response.json();
     })
     .then(function (data) {
-        console.log(data);
-        var weatherConditionArray = (data.weather[0])
-        console.log(weatherConditionArray)
-        
-        var weatherDataDescription = weatherConditionArray.description
-        console.log(weatherDataDescription)
-        weatherEl.empty().append(weatherDataDescription)
+      console.log(data);
+      var weatherConditionArray = data.weather[0];
+      console.log(weatherConditionArray);
 
-    
-        icon = weatherConditionArray.icon
-        
+      var weatherDataDescription = weatherConditionArray.description;
+      console.log(weatherDataDescription);
+      weatherEl.empty().append(weatherDataDescription);
+
+      icon = weatherConditionArray.icon;
     });
-
-
- 
-
-
-
-
-
 }
-getApi()
+getApi();
 
-searchEl.on("submit",userInputCitySubmit);
+searchEl.on("submit", userInputCitySubmit);

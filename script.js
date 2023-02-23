f9b85450dd842ad1c0c7cd32a788a47b98cb91ec
James Geneser <jamesgeneser@gmail.com>
2023-02-22 20:30:45 -0700

    created the weatherConditions div container. Added openWeather API functionality to append current weather conditions to the page

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ var city = "Denver"
 var APIKey = "22c9b7b3a4e8a1f95ebc91f63d87daea"
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" +APIKey;
 var weatherConditions
-
-
+var mainWeatherEl = $("#weatherConditions")
+var subMainWeatherEl = $("#subDescription")
+var icon = $(".icon")
 
 function getApi(){
     fetch(queryURL)
@@ -18,15 +19,16 @@ function getApi(){
     })
     .then(function (data) {
         console.log(data);
-        weatherConditions = (data.weather)
-        console.log(weatherConditions)
-        var weatherEl = weatherConditions[0]
-        console.log(weatherEl)
-        var mainWeatherEl = weatherEl.main
-        var subMainWeatherEl = weatherEl.description
-        var icon = weatherEl.icon
+        var weatherConditionArray = (data.weather[0])
+        console.log(weatherConditionArray)
+        
+        var weatherDataDescription = weatherConditionArray.description
+        console.log(weatherDataDescription)
+        mainWeatherEl.append("<h3>"+weatherDataDescription+"</h3>")
 
-
+    
+        icon = weatherConditionArray.icon
+        
     });
 
 
